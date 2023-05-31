0e55a7bd75fc74367c127590ffe67036ff7dd163
Song S <129936802+SongShienSongShiQuan@users.noreply.github.com>
2023-05-31 23:35:15 +0800

    Add files via upload

---
+++ MainWindow(Digital_Clock).xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Digital_Clock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+		{
+			InitializeComponent();
+            this.ResizeMode = ResizeMode.NoResize;
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+            this.Topmost = true;
+            DispatcherTimer timer = new DispatcherTimer();
+			timer.Interval = TimeSpan.FromSeconds(1);
+			timer.Tick += timer_Tick;
+			timer.Start();
+		}
+
+		void timer_Tick(object sender, EventArgs e)
+		{
+			Label1.Content = DateTime.Now.ToString("HH:mm:ss.fff");
+		}
+    }
+}
