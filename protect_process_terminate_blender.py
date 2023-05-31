15d93264968b7aaa0a72ddfd8f1500cc94af9b0b
Song S <129936802+SongShienSongShiQuan@users.noreply.github.com>
2023-05-31 23:07:21 +0800

    Update protect_process_terminate_blender.py

--- protect_process_terminate_blender.py
+++ protect_process_terminate_blender.py
@@ -10,7 +10,10 @@ import subprocess
 
 print('Initializing...')
 print('If RAM usage exceeded: Terminate \'blender.exe\' to protect_process.')
+
 protect_process = True
+interation = 0
+print_limiter = 5
 while protect_process == True:
     get_ram_usage = psutil.virtual_memory()
     get_ram_usage_percent = get_ram_usage.percent.conjugate()
@@ -30,11 +33,21 @@ while protect_process == True:
             length_read_file_init = (len(read_file_init))
             if length_output_process >= ((length_read_file_init)-5) and length_output_process <= ((length_read_file_init)+5):
                 print('Process Terminated', output_process)
-                print('Initializing: Sys.ext()')
-                sys.exit()
+                add_interation = interation + 1
+                interation = add_interation
+                print(interation)
+                if interation == 5:
+                    print(output_process)
+                    print('All Process Terminated')
+                    print('Initializing sys.exit()...')
+                    sys.exit()
             print('Re-init...')
             
 
     elif get_ram_usage_percent < ram_usage_var1:
         time.sleep(5)
-        print('RAM usage normal.')
+        if print_limiter > 0:
+            print_limiter_sub = print_limiter -1
+            print_limiter = print_limiter_sub
+            print_limiter_to_str = str(print_limiter)
+            print('RAM usage normal. Checks Left: '+print_limiter_to_str+' protect_process_terminate_blender.py will still run after checks is not printing.')
